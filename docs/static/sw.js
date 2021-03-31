1cb2a08e62502196412b62e0db9d2ab6e4416d64
Alex Lockhart <alexlockahrt7@gmail.com>
2021-03-31 04:55:32 -0400

    Added some html injection safety

--- docs/static/sw.js
+++ docs/static/sw.js
@@ -1,4 +1,4 @@
-var appCacheName = 'app-cache-v19';
+var appCacheName = 'app-cache-v20';
 var cacheNames = [appCacheName];
 var offlinePage = '/offline.html';
 var offlineFragment = '/offline.fragment.html';
--- src/services/html.js
+++ src/services/html.js
@@ -1,5 +1,11 @@
 import weave from './weave.js';
 
 export default function html(parts, ...expressions) {
-    return weave(parts, expressions).join('');
+    return weave(
+        parts,
+        expressions.map(exp => exp
+            .replaceAll('<', '&lt;')
+            .replaceAll("'", '&apos;')
+            .replaceAll('"', '&squot;'))
+    ).join('');
 }
